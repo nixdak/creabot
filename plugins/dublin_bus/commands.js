60f458b0128adbff00376f7ca1699858c6193bda
Craig Gavagan <craig.gavagan@gmail.com>
2015-06-02 03:19:34 +0100

    fix typo in plugins/dublin_bus/commands.js

--- plugins/dublin_bus/commands.js
+++ plugins/dublin_bus/commands.js
@@ -1,7 +1,7 @@
 var DublinBusInfo = require('./controllers/dublin_bus_info.js');
 
 module.exports = function(app) {
-    var dublinBusInfo= new dublinBusInfo();
+    var dublinBusInfo= new DublinBusInfo();
 
     app.cmd('dbus', '', dublinBusInfo.showStopInfo);
 }
