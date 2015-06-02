0970274e00157e843530255c1cb20cf11d8b163f
Craig Gavagan <craig.gavagan@gmail.com>
2015-06-02 04:07:08 +0100

    Fix reference to joinChannel in plugin_code/dublin_bus/setup.js

--- plugin_code/dublin_bus/setup.js
+++ plugin_code/dublin_bus/setup.js
@@ -3,6 +3,6 @@ var DublinBusInfo = require('./controllers/dublin_bus_info.js');
 module.exports = function(app) {
     var dublinBusInfo = new DublinBusInfo();
 
-    app.join(dublinBusInfo.config.channelsToJoin);
+    app.joinChannel(dublinBusInfo.config.channelsToJoin);
     app.cmd('dbus', '', dublinBusInfo.config.channels, dublinBusInfo.showStopInfo);
 }
