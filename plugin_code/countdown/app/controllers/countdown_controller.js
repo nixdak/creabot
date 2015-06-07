5d2265d3f7296a9ba58cf7cbb8197536102f19fb
Craig Gavagan <craig.gavagan@gmail.com>
2015-06-07 22:50:15 +0100

    Missing paren

--- plugin_code/countdown/app/controllers/countdown_controller.js
+++ plugin_code/countdown/app/controllers/countdown_controller.js
@@ -31,7 +31,7 @@ var Countdown = function Countdown() {
 
   self.challenge = function (client, message, cmdArgs) {
     var channel = message.args[0];
-    if (!_.contains(self.challeneges, { challenger: message.nick, challenged: cmdArgs[0] }) {
+    if (!_.contains(self.challeneges, { challenger: message.nick, challenged: cmdArgs[0] })) {
       self.challenges.push({ challenger: message.nick, challenged: cmdArgs[0] });
       client.say(channel, message.nick: + ' has challenged ' + cmdArgs[0]);
       client.say(channel, cmdArgs[0] + ': To accept ' + message.nick + '\'s challenge, simply !accept ' + message.nick);
