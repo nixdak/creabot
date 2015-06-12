2a5749cad77f99f7b3b9a3fdf0357b62820cf636
Craig Gavagan <craig.gavagan@gmail.com>
2015-06-13 00:40:07 +0100

    Remove unecessary commands

--- plugin_code/countdown/setup.js
+++ plugin_code/countdown/setup.js
@@ -14,11 +14,9 @@ module.exports = function(app) {
   app.cmd('join', '', countdown.config.pluginOptions.channels, countdown.config.pluginOptions.channelsToExclude, countdown.join);
   app.cmd('list', '', countdown.config.pluginOptions.channels, countdown.config.pluginOptions.channelsToExclude, countdown.list);
   //app.cmd('lock', '', countdown.config.pluginOptions.channels, countdown.config.pluginOptions.channelsToExclude, countdown.list);
-  //app.cmd('players', '', countdown.config.pluginOptions.channels, contdown.config.pluginOptions.channelsToExclude, countdown.players);
   app.cmd('quit', '', countdown.config.pluginOptions.channels, countdown.config.pluginOptions.channelsToExclude, countdown.stop);
   app.cmd('select', '', countdown.config.pluginOptions.channels, countdown.config.pluginOptions.channelsToExclude, countdown.select);
   app.cmd('cd', '', countdown.config.pluginOptions.channels, countdown.config.pluginOptions.channelsToExclude, countdown.select);
-  //app.cmd('start', '', countdown.config.pluginOptions.channels, countdown.config.pluginOptions.channelsToExclude, countdown.start);
   //app.cmd('status', '', countdown.config.pluginOptions.channels, countdown.config.pluginOptions.channelsToExclude, countdown.status);
   app.cmd('stop', '', countdown.config.pluginOptions.channels, countdown.config.pluginOptions.channelsToExclude, countdown.stop);
 
