16a72ad13e7cff608bbc020906d47faa77038bea
Cian Butler <cianbutlerx@gmail.com>
2015-06-26 22:43:40 +0100

    closes #70

--- plugin_code/countdown/app/controllers/countdown_controller.js
+++ plugin_code/countdown/app/controllers/countdown_controller.js
@@ -51,6 +51,8 @@ var Countdown = function Countdown() {
       client.say(channel, 'Please supply a nick with this command');
     } else if (client.nick.toLowerCase() === cmdArgs.toLowerCase()) {
       client.say(channel, 'You can\'t challenge the bot');
+    } else if (message.nick.toLowerCase() === cmdArgs.toLowerCase()){
+      client.say(channel, 'You can\'t challenge yourself');
     }else if (!_.contains(self.challeneges, { challenger: message.nick, challenged: cmdArgs })) {
       self.challenges.push({ challenger: message.nick, challenged: cmdArgs });
       client.say(channel, message.nick + ': has challenged ' + cmdArgs);
