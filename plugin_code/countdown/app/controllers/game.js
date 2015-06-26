74f2af8994b79104c17d5ccc8c10cf1869e218a1
Cian Butler <butlerx@notthe.cloud>
2015-06-26 23:15:56 +0100

    change how check for null

--- plugin_code/countdown/app/controllers/game.js
+++ plugin_code/countdown/app/controllers/game.js
@@ -93,7 +93,7 @@ var Game = function Game(channel, client, config, challenger, challenged) {
 
     if (self.challenger.nick === player || self.challenged.nick === player) {
       self.say(player + ' stopped the game.');
-    } else if (player !== '') {//check if its null
+    } else if (!_.isUndefined(player)) {//check if its null
       return false;
     }
 
