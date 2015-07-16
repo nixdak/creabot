855dafb038ae1bfef7c69639c51bdae49de28458
Craig Gavagan <craig.gavagan@gmail.com>
2015-07-16 21:50:39 +0100

    Skeleton of uno controller

--- plugin_code/uno/setup.js
+++ plugin_code/uno/setup.js
@@ -7,10 +7,14 @@ module.exports = function(app) {
   app.joinChannels(uno.config.pluginOptions.channelsToJoin);
 
   // Public commands
+  app.cmd('challenge', '', uno.config.pluginOptions.channels, uno.config.pluginOptions.channelsToExclude, uno.challenge);
   app.cmd('draw', '', uno.config.pluginOptions.channels, uno.config.pluginOptions.channelsToExclude, uno.draw);
   app.cmd('j', '', uno.config.pluginOptions.channels, uno.config.pluginOptions.channelsToExclude, uno.join);
   app.cmd('join', '', uno.config.pluginOptions.channels, uno.config.pluginOptions.channelsToExclude, uno.join);
   app.cmd('quit', '', uno.config.pluginOptions.channels, uno.config.pluginOptions.channelsToExclude, uno.quit);
+  app.cmd('score', '', uno.config.pluginOptions.channels, uno.config.pluginOptions.channelsToExclude, uno.score);
+  app.cmd('start', '', uno.config.pluginOptions.channels, uno.config.pluginOptions.channelsToExclude, uno.start);
+  app.cmd('stop', '', uno.config.pluginOptions.channels, uno.config.pluginOptions.channelsToExclude, uno.stop);
   app.cmd('uno', '', uno.config.pluginOptions.channels, uno.config.pluginOptions.channelsToExclude, uno.uno);
 
   // Private commands
