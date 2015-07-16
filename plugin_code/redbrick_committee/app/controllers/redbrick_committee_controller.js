1df7d1a5a0716c359de3c7c29f3b4a056f9bc652
Craig Gavagan <craig.gavagan@gmail.com>
2015-07-16 11:55:11 +0100

    Make controller modular
    Intention is to have each position be a trigger.

--- plugin_code/redbrick_committee/app/controllers/redbrick_committee_controller.js
+++ plugin_code/redbrick_committee/app/controllers/redbrick_committee_controller.js
@@ -12,63 +12,82 @@ var RedbrickCommittee = function RedbrickCommittee() {
   self.showCommitteeInfo = function(client, message, cmdArgs) {
     var nick = message.nick;
 
-    // Show chairperson
+    self.showChair(message.args[0]);
+    self.showSecretary(message.args[0]);
+    self.showTreasurer(message.args[0]);
+    self.showPRO(message.args[0]);
+    self.showEvents(message.args[0]);
+    self.showFYR(message.args[0]);
+    self.showWebmaster(message.args[0]);
+    self.showHelpdesk(message.args[0]);
+    self.showAdmins(message.args[0]);
+  };
+
+  self.showChair = function (channel) {
     var chairperson = _.find(self.committee, { role: 'Chairperson' });
     if (!_.isUndefined(chairperson)) {
       chair_string = chairperson.name + ' (' + chairperson.nick + ')';
-      client.say(nick, 'Chairperson: ' + chair_string);
+      client.say(channel, 'Chairperson: ' + chair_string);
     }
+  };
 
-    // Show secretary
+  self.showSecretary = function (channel) {
     var secretary = _.find(self.committee, { role: 'Secretary' });
     if (!_.isUndefined(secretary)) {
       secretary_string = secretary.name + ' (' + secretary.nick + ')';
       client.say(nick, 'Secretary: ' + secretary_string);
     }
+  };
 
-    // Show treasurer
+  self.showTreasurer = function (channel) {
     var treasurer = _.find(self.committee, { role: 'Treasurer' });
     if (!_.isUndefined(treasurer)) {
       treasurer_string = treasurer.name + ' (' + treasurer.nick + ')';
       client.say(nick, 'Treasurer: ' + treasurer_string);
     }
+  };
 
-    // Show public relations officer
+  self.showPRO = function (channel) {
     var pro = _.find(self.committee, { role: 'Public Relations Officer' });
     if (!_.isUndefined(pro)) {
       pro_string = pro.name + ' (' + pro.nick + ')';
       client.say(nick, 'Public Relations Officer: ' + pro_string);
     }
+  };
 
-    // Show Events officer
+  self.showEvents = function(channel) {
     var events = _.find(self.committee, { role: 'Events Officer' });
     if (!_.isUndefined(events)) {
       events_string = events.name + ' (' + events.nick + ')';
       client.say(nick, 'Events Officer: ' + events_string);
     }
+  };
 
-    // Show first year rep
+  self.showFYR = function(channel) {
     var first_year_rep = _.find(self.committee, { role: 'First Year Representative' });
     if (!_.isUndefined(first_year_rep)) {
       fyr_string = first_year_rep.name + ' (' + first_year_rep.nick + ')';
       client.say(nick, 'First Year Representative: ' + fyr_string);
     }
+  };
 
-    // Show webmaster
+  self.showWebmaster = function (channel) {
     var webmaster = _.find(self.committee, { role: 'Webmaster' });
     if (!_.isUndefined(webmaster)) {
       webmaster_string = webmaster.name + ' (' + webmaster.nick + ')';
       client.say(nick, 'Webmaster: ' + webmaster_string);
     }
+  };
 
-    // Show helpdesk
+  self.showHelpdesk = function (channel) {
     var helpdesk = _.filter(self.committee, { role: 'Helpdesk' });
     if (!_.isUndefined(helpdesk)) {
       var helpdesk_string = _.map(helpdesk, function (member) { return member.name + ' (' + member.nick + ')' }).join(', ');
       client.say(nick, 'Helpdesk: ' + helpdesk_string);
     }
+  };
 
-    // Show admins
+  self.admins = function (channel) {
     var admins = _.filter(self.committee, { role: 'System Administrator' });
     if (!_.isUndefined(admins)) {
       var admins_string = _.map(admins, function (member) { return member.name + ' (' + member.nick + ')' }).join(', ');
