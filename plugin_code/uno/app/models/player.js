2d802cb9ebf29094280e07a38bce527d6772c65d
Craig Gavagan <craig.gavagan@gmail.com>
2015-07-18 01:00:43 +0100

    Add player, card models, uno card json file

---
+++ plugin_code/uno/app/models/player.js
@@ -0,0 +1,16 @@
+var Deck = require('../controllers/deck.js');
+
+var Player = function(nick, user, hostname) {
+  var self = this;
+
+  self.nick = nick;
+  self.user = user;
+  self.hostname = hostname;
+  self.points = 0;
+  self.isActive = true;
+  self.hand = new Deck();
+  self.hasPlayed = false;
+  self.hasDrawn = false;
+};
+
+exports = module.exports = Player;
