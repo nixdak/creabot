263b048f2e551663482bbd2a41f52f368a3e760e
Cian Butler <butlerx@GreyBeard.local>
2015-07-27 22:21:02 +0100

    closes #42

--- plugin_code/countdown/app/controllers/countdown_controller.js
+++ plugin_code/countdown/app/controllers/countdown_controller.js
@@ -3,12 +3,13 @@ var _ = require('underscore'),j
     Player = require('../models/player'),
     env = process.env.NODE_ENV || 'development',
     config = require('../../config/config.json')[env];
+    challenges = require('../../config/challenges.json')
 
 var Countdown = function Countdown() {
   var self = this;
   self.game;
   self.config = config;
-  self.challenges = config.gameOptions.challenges;
+  self.challenges = challenges.games;
 
   self.accept = function (client, message, cmdArgs) {
     if (_.isUndefined(self.game) || self.game.state === Game.STATES.STOPPED) {
@@ -92,6 +93,7 @@ var Countdown = function Countdown() {
         }
       }
       self.challenges.push({ challenger: message.nick, challenged: args[0], letter: letterTime, number: numberTime, conundrum: conundrumTime});
+      fs.writeFile(challenges, JSON.stringify(self.challenges, null, 2);
       client.say(channel, message.nick + ': has challenged ' + args[0]);
       client.say(channel, args[0] + ': To accept ' + message.nick + '\'s challenge, simply !accept ' + message.nick);
     } else {
@@ -106,6 +108,7 @@ var Countdown = function Countdown() {
       self.challenges = _.reject(self.challenges, function(challenge) {
         return challenge.challenger === self.game.challenger.nick && challenge.challenged === self.game.challenged.nick;
       });
+      fs.writeFile(challenges, JSON.stringify(self.challenges, null, 2);
     } else {
       client.say(message.args[0], 'Unable to join at the moment.');
     }
