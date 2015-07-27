4f7f4c6f13c9396540a3871235a7954adecb3999
Craig Gavagan <craig.gavagan@gmail.com>
2015-07-27 13:30:26 +0100

    card.toString()

--- plugin_code/uno/app/models/card.js
+++ plugin_code/uno/app/models/card.js
@@ -1,3 +1,5 @@
+var c = require('irc-colors');
+
 var Card = function Card(card) {
   var self = this;
 
@@ -49,6 +51,49 @@ var Card = function Card(card) {
   self.wildDrawFour = function () {
 
   };
+
+  self.toString = function () {
+    var cardString = '';
+
+    switch (self.type) {
+      case: 'Number':
+        cardString = self.color + ' ' + self.value;
+        break;
+      case: 'Skip':
+        cardString = self.color + ' Skip';
+        break;
+      case: 'Reverse':
+        cardString = self.color + ' Reverse';
+        break;
+      case: 'Draw Two':
+        cardString = self.color + ' Draw Two';
+        break;
+      case: 'Wild':
+        cardString = self.color + ' Wild';
+        break;
+      case: 'Wild Draw Four':
+        cardStrign = self.color + ' Wild Draw Four';
+        break;
+    }
+
+    switch (self.color) {
+      case: 'yellow':
+        cardString = c.bold.yellow(cardString);
+        break;
+      case: 'green':
+        cardString = c.bold.green(cardString);
+        break;
+      case: 'blue':
+        cardString = c.bold.blue(cardString);
+        break;
+      case: 'red':
+        cardString = c.bold.red(cardString);
+        break;
+      case: 'wild':
+        cardString = c.bold.brown(cardString);
+        break;
+    }
+  }
 }
 
 exports = module.exports = Card;
