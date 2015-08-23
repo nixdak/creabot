479af5d8025330632bc539bda6e1e207a2fefab4
Craig Gavagan <craig.gavagan@gmail.com>
2015-08-23 11:34:54 +0100

    Mismatched brackets

--- plugin_code/uno/app/models/game.js
+++ plugin_code/uno/app/models/game.js
@@ -89,7 +89,7 @@ var Game = function (channel, client, config, cmdArgs) {
   };
 
   self.nextPlayer = function() {
-    for (var i = (self.players.indexOf(self.currentPlayer + 1) % self.players.length; i !== self.players.indexOf(self.currentPlayer); i = (i + 1) % self.players.legnth) {
+    for (var i = (self.players.indexOf(self.currentPlayer + 1) % self.players.length); i !== self.players.indexOf(self.currentPlayer); i = (i + 1) % self.players.legnth) {
       if (self.players[i].isActive === true && self.players[i].skipped === false) {
 	return self.players[i];
       }
