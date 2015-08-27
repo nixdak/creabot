3fd91d5a77aaf9159db6506bedaedc67543d7393
Cian Butler <butlerx@redbrick.dcu.ie>
2015-08-27 22:16:15 +0100

    disable !challenge till code is finished

--- plugin_code/uno/setup.js
+++ plugin_code/uno/setup.js
@@ -8,7 +8,7 @@ module.exports = function(app) {
 
   // Public commands
   app.cmd('cards', '', uno.config.pluginOptions.channels, uno.config.pluginOptions.channelsToExclude, uno.cards);
-  app.cmd('challenge', '', uno.config.pluginOptions.channels, uno.config.pluginOptions.channelsToExclude, uno.challenge);
+  //app.cmd('challenge', '', uno.config.pluginOptions.channels, uno.config.pluginOptions.channelsToExclude, uno.challenge);
   app.cmd('j', '', uno.config.pluginOptions.channels, uno.config.pluginOptions.channelsToExclude, uno.join);
   app.cmd('join', '', uno.config.pluginOptions.channels, uno.config.pluginOptions.channelsToExclude, uno.join);
   app.cmd('quit', '', uno.config.pluginOptions.channels, uno.config.pluginOptions.channelsToExclude, uno.quit);
