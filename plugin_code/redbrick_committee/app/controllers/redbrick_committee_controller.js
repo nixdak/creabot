de7e33aa0f0a8e3a54dd8e8ac0f03f13841dafea
Cian Butler <butlerx@redbrick.dcu.ie>
2015-12-18 12:26:44 +0000

    start #143

--- plugin_code/redbrick_committee/app/controllers/redbrick_committee_controller.js
+++ plugin_code/redbrick_committee/app/controllers/redbrick_committee_controller.js
@@ -29,98 +29,115 @@ var RedbrickCommittee = function RedbrickCommittee() {
 
   self.showCommitteeInfo = function(client, message, cmdArgs) {
     var nick = message.nick;
-    self.client = client;
 
-    self.showChair(message.args[0]);
-    self.showSecretary(message.args[0]);
-    self.showTreasurer(message.args[0]);
-    self.showPRO(message.args[0]);
-    self.showEvents(message.args[0]);
-    self.showFYR(message.args[0]);
-    self.showWebmaster(message.args[0]);
-    self.showHelpdesk(message.args[0]);
-    self.showAdmins(message.args[0]);
+    self.showChair(client, message, cmdArgs, true);
+    self.showSecretary(client, message, cmdArgs, true);
+    self.showTreasurer(client, message, cmdArgs, true);
+    self.showPRO(client, message, cmdArgs, true);
+    self.showEvents(client, message, cmdArgs, true);
+    self.showFYR(client, message, cmdArgs, true);
+    self.showWebmaster(client, message, cmdArgs, true);
+    self.showHelpdesk(client, message, cmdArgs, true);
+    self.showAdmins(client, message, cmdArgs, true);
     self.wait();
   };
 
-  self.showChair = function (channel) {
+  self.showChair = function (client, message, cmdArgs, cmt) {
     var chairperson = _.find(self.committee, { role: 'Chairperson' });
     if (!_.isUndefined(chairperson) && self.state === STATES.READY) {
       chair_string = chairperson.name + ' (' + chairperson.nick + ')';
-      self.client.say(channel, 'Chairperson: ' + chair_string);
-      // self.wait();
+      client.say(message.args[0], 'Chairperson: ' + chair_string);
+      if (cmt !== true) {
+        self.wait();
+      }
     }
   };
 
-  self.showSecretary = function (channel) {
+  self.showSecretary = function (client, message, cmdArgs) {
     var secretary = _.find(self.committee, { role: 'Secretary' });
     if (!_.isUndefined(secretary) && self.state === STATES.READY) {
       secretary_string = secretary.name + ' (' + secretary.nick + ')';
-      self.client.say(channel, 'Secretary: ' + secretary_string);
-      // self.wait();
+      client.say(message.args[0], 'Secretary: ' + secretary_string);
+      if (cmt !== true) {
+        self.wait();
+      }
     }
   };
 
-  self.showTreasurer = function (channel) {
+  self.showTreasurer = function (client, message, cmdArgs) {
     var treasurer = _.find(self.committee, { role: 'Treasurer' });
     if (!_.isUndefined(treasurer) && self.state === STATES.READY) {
       treasurer_string = treasurer.name + ' (' + treasurer.nick + ')';
-      self.client.say(channel, 'Treasurer: ' + treasurer_string);
-      // self.wait();
+      client.say(message.args[0], 'Treasurer: ' + treasurer_string);
+      if (cmt !== true) {
+        self.wait();
+      }
     }
   };
 
-  self.showPRO = function (channel) {
+  self.showPRO = function (client, message, cmdArgs) {
     var pro = _.find(self.committee, { role: 'Public Relations Officer' });
     if (!_.isUndefined(pro) && self.state === STATES.READY) {
       pro_string = pro.name + ' (' + pro.nick + ')';
-      self.client.say(channel, 'Public Relations Officer: ' + pro_string);
-      // self.wait();
+      client.say(message.args[0], 'Public Relations Officer: ' + pro_string);
+      if (cmt !== true) {
+        self.wait();
+      }
     }
   };
 
-  self.showEvents = function(channel) {
+  self.showEvents = function(client, message, cmdArgs) {
     var events = _.find(self.committee, { role: 'Events Officer' });
     if (!_.isUndefined(events) && self.state === STATES.READY) {
       events_string = events.name + ' (' + events.nick + ')';
-      self.client.say(channel, 'Events Officer: ' + events_string);
-      // self.wait();
+      client.say(message.args[0], 'Events Officer: ' + events_string);
+      if (cmt !== true) {
+        self.wait();
+      }
     }
   };
 
-  self.showFYR = function(channel) {
+  self.showFYR = function(client, message, cmdArgs) {
     var first_year_rep = _.find(self.committee, { role: 'First Year Representative' });
     if (!_.isUndefined(first_year_rep) && self.state === STATES.READY) {
       fyr_string = first_year_rep.name + ' (' + first_year_rep.nick + ')';
-      self.client.say(channel, 'First Year Representative: ' + fyr_string);
-      // self.wait();
+      client.say(message.args[0], 'First Year Representative: ' + fyr_string);
+      if (cmt !== true) {
+        self.wait();
+      }
     }
   };
 
-  self.showWebmaster = function (channel) {
+  self.showWebmaster = function (client, message, cmdArgs) {
     var webmaster = _.find(self.committee, { role: 'Webmaster' });
     if (!_.isUndefined(webmaster) && self.state === STATES.READY) {
       webmaster_string = webmaster.name + ' (' + webmaster.nick + ')';
-      self.client.say(channel, 'Webmaster: ' + webmaster_string);
-      // self.wait();
+      client.say(message.args[0], 'Webmaster: ' + webmaster_string);
+      if (cmt !== true) {
+        self.wait();
+      }
     }
   };
 
-  self.showHelpdesk = function (channel) {
+  self.showHelpdesk = function (client, message, cmdArgs) {
     var helpdesk = _.filter(self.committee, { role: 'Helpdesk' });
     if (!_.isUndefined(helpdesk) && self.state === STATES.READY) {
       var helpdesk_string = _.map(helpdesk, function (member) { return member.name + ' (' + member.nick + ')' }).join(', ');
-      self.client.say(channel, 'Helpdesk: ' + helpdesk_string);
-      // self.wait();
+      client.say(message.args[0], 'Helpdesk: ' + helpdesk_string);
+      if (cmt !== true) {
+        self.wait();
+      }
     }
   };
 
-  self.showAdmins = function (channel) {
+  self.showAdmins = function (client, message, cmdArgs) {
     var admins = _.filter(self.committee, { role: 'System Administrator' });
     if (!_.isUndefined(admins) && self.state === STATES.READY) {
       var admins_string = _.map(admins, function (member) { return member.name + ' (' + member.nick + ')' }).join(', ');
-      self.client.say(channel, 'System Administrators: ' + admins_string);
-      // self.wait();
+      client.say(message.args[0], 'System Administrators: ' + admins_string);
+      if (cmt !== true) {
+        self.wait();
+      }
     }
   };
 
