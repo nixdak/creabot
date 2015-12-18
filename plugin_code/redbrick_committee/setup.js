09a6f42e1d06911af221e75e6b74541b4602e6f2
Cian Butler <butlerx@redbrick.dcu.ie>
2015-12-18 11:48:24 +0000

    commented out channel commands and moved wait to cmt command

--- plugin_code/redbrick_committee/setup.js
+++ plugin_code/redbrick_committee/setup.js
@@ -8,14 +8,14 @@ module.exports = function(app) {
 
     // Add commands
     app.cmd('cmt', '', redbrickCommittee.config.channels, redbrickCommittee.config.channelsToExclude, redbrickCommittee.showCommitteeInfo);
-    app.cmd('chair', '', redbrickCommittee.config.channels, redbrickCommittee.config.channelsToExclude, redbrickCommittee.showChair);
-    app.cmd('treasurer', '', redbrickCommittee.config.channels, redbrickCommittee.config.channelsToExclude, redbrickCommittee.showTreasurer);
-    app.cmd('secretary', '', redbrickCommittee.config.channels, redbrickCommittee.config.channelsToExclude, redbrickCommittee.showSecretary);
-    app.cmd('pro', '', redbrickCommittee.config.channels, redbrickCommittee.config.channelsToExclude, redbrickCommittee.showPRO);
-    app.cmd('events', '', redbrickCommittee.config.channels, redbrickCommittee.config.channelsToExclude, redbrickCommittee.showEvents);
-    app.cmd('helpdesk', '', redbrickCommittee.config.channels, redbrickCommittee.config.channelsToExclude, redbrickCommittee.showHelpdesk);
-    app.cmd('admins', '', redbrickCommittee.config.channels, redbrickCommittee.config.channelsToExclude, redbrickCommittee.showAdmins);
-    app.cmd('webmaster', '', redbrickCommittee.config.channels, redbrickCommittee.config.channelsToExclude, redbrickCommittee.showWebmaster);
-    app.cmd('fyr', '', redbrickCommittee.config.channels, redbrickCommittee.config.channelsToExclude, redbrickCommittee.showFYR);
+    // app.cmd('chair', '', redbrickCommittee.config.channels, redbrickCommittee.config.channelsToExclude, redbrickCommittee.showChair);
+    // app.cmd('treasurer', '', redbrickCommittee.config.channels, redbrickCommittee.config.channelsToExclude, redbrickCommittee.showTreasurer);
+    // app.cmd('secretary', '', redbrickCommittee.config.channels, redbrickCommittee.config.channelsToExclude, redbrickCommittee.showSecretary);
+    // app.cmd('pro', '', redbrickCommittee.config.channels, redbrickCommittee.config.channelsToExclude, redbrickCommittee.showPRO);
+    // app.cmd('events', '', redbrickCommittee.config.channels, redbrickCommittee.config.channelsToExclude, redbrickCommittee.showEvents);
+    // app.cmd('helpdesk', '', redbrickCommittee.config.channels, redbrickCommittee.config.channelsToExclude, redbrickCommittee.showHelpdesk);
+    // app.cmd('admins', '', redbrickCommittee.config.channels, redbrickCommittee.config.channelsToExclude, redbrickCommittee.showAdmins);
+    // app.cmd('webmaster', '', redbrickCommittee.config.channels, redbrickCommittee.config.channelsToExclude, redbrickCommittee.showWebmaster);
+    // app.cmd('fyr', '', redbrickCommittee.config.channels, redbrickCommittee.config.channelsToExclude, redbrickCommittee.showFYR);
     app.cmd('reload', '', redbrickCommittee.config.channels, redbrickCommittee.config.channelsToExclude, redbrickCommittee.reload);
 }
