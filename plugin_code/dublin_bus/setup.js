d2a3efdd2264756b35b07386e37ac84e2eaa6225
Cian Butler <butlerx@redbrick.dcu.ie>
2015-12-20 23:15:51 +0000

    remove unused code

--- plugin_code/dublin_bus/setup.js
+++ plugin_code/dublin_bus/setup.js
@@ -1,10 +1,7 @@
 var DublinBusInfo = require('./app/controllers/dublin_bus_info.js');
-var Popping = require('../popping/app/controllers/popping.js');
-
 
 module.exports = function(app) {
   var dublinBusInfo = new DublinBusInfo();
-  var popping = new Popping();
 
   // Join Channels
   app.joinChannels(dublinBusInfo.config.channelsToJoin);
@@ -12,5 +9,4 @@ module.exports = function(app) {
   // Add commands
   app.cmd('dbus', '', dublinBusInfo.config.channels, dublinBusInfo.config.channelsToExclude, dublinBusInfo.showStopInfo);
   app.cmd('stop', '', dublinBusInfo.config.channels, dublinBusInfo.config.channelsToExclude, dublinBusInfo.showStopInfo);
-  app.cmd('pop', '', popping.config.channels, popping.config.channelsToExclude, popping.pop);
-}
+};
