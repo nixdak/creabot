e8b1a95830130bb6cad4018c9ebc6481d6d74242
Cian Butler <butlerx@redbrick.dcu.ie>
2015-12-20 23:20:18 +0000

    testing

--- plugin_code/dublin_bus/setup.js
+++ plugin_code/dublin_bus/setup.js
@@ -1,12 +1,14 @@
 var DublinBusInfo = require('./app/controllers/dublin_bus_info.js');
+var Popping = require('./app/controllers/popping.js');
 
 module.exports = function(app) {
   var dublinBusInfo = new DublinBusInfo();
+  var popping = new Popping();
 
   // Join Channels
   app.joinChannels(dublinBusInfo.config.channelsToJoin);
 
   // Add commands
   app.cmd('dbus', '', dublinBusInfo.config.channels, dublinBusInfo.config.channelsToExclude, dublinBusInfo.showStopInfo);
-  app.cmd('stop', '', dublinBusInfo.config.channels, dublinBusInfo.config.channelsToExclude, dublinBusInfo.showStopInfo);
+  app.cmd('stop', '', dublinBusInfo.config.channels, dublinBusInfo.config.channelsToExclude, popping.pop);
 };
