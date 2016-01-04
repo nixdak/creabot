cf5a9308c804b6bd5f729dfd297664064b218963
butlerx <cianbutlerx@gmail.com>
2016-01-04 21:15:47 +0000

    refer to title

--- plugin_code/bookclub/setup.js
+++ plugin_code/bookclub/setup.js
@@ -10,10 +10,10 @@ module.exports = function(app) {
   app.cmd('suggest', '', bookclub.config.channels, bookclub.config.channelsToExclude, bookclub.suggest);
   app.cmd('current', '', bookclub.config.channels, bookclub.config.channelsToExclude, bookclub.thisMonth);
   app.cmd('next', '', bookclub.config.channels, bookclub.config.channelsToExclude, bookclub.nextMonth);
-  app.cmd('listbook', '', bookclub.config.channels, bookclub.config.channelsToExclude, bookclub.showBooks);
+  app.cmd('listbooks', '', bookclub.config.channels, bookclub.config.channelsToExclude, bookclub.showBooks);
   app.cmd('listread', '', bookclub.config.channels, bookclub.config.channelsToExclude, bookclub.showRead);
 
   // Private commands
-  app.msg('listbook', '', bookclub.showBooks);
+  app.msg('listbooks', '', bookclub.showBooks);
   app.msg('listread', '', bookclub.showRead);
 };
