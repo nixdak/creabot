db43910964a170696cea03d3037e4ed44623bb5a
butlerx <butlerx@redbrick.dcu.ie>
2016-01-07 14:19:28 +0000

    change how get webpage

--- plugin_code/helpdesk/app/controllers/Helpdesk.js
+++ plugin_code/helpdesk/app/controllers/Helpdesk.js
@@ -1,7 +1,8 @@
 var fs = require('fs'),
     c = require('irc-colors'),
     _ = require('underscore'),
-    tabletojson = require('tabletojson'),
+    request = require('request'),
+    cherrio = require('cherrio'),
     env = process.env.NODE_ENV || 'development',
     config = require('../../config/config.json')[env];
 
@@ -11,14 +12,21 @@ var Helpdesk = function Helpdesk() {
 
   self.help = function (client, message, cmdArgs) {
     var input = cmdArgs.split(" ", 1);
-      if (input[0] === '') {
-        client.say(message.args[0], 'Helpdesk is a bot to help with all your problems pm me !help for a list of commads');
-        return false;
+    if (input[0] === '') {
+      client.say(message.args[0], 'Helpdesk is a bot to help with all your problems pm me !help for a list of commads');
+      return false;
+    }
+    var url = 'http://wiki.redbrick.dcu.ie/mw/' + input[0];
+    request(url, function(error, response, html){
+      if(!error){
+        var $ = cheerio.load(html);
+        // We'll use the unique header class as a starting point.
+        $('.header').filter(function(){
+        // Let's store the data we filter into a variable so we can easily see what's going on.
+        var data = $(this);
+        console(data);
       }
-      var url = 'http://wiki.redbrick.dcu.ie/mw/' + input[0];
-      tabletojson.convertUrl(url).then(function (tablesAsJson) {
-        console.log(tablesAsJson);
-      })
+    })  
   };
 
   self.list = function (client, message, cmdArgs) {
