891551cf13fe0321ce1176cf2c9b53ee5b3e96eb
butlerx <cianbutlerx@gmail.com>
2016-01-08 20:15:40 +0000

    close #27

--- plugin_code/helpdesk/setup.js
+++ plugin_code/helpdesk/setup.js
@@ -8,8 +8,10 @@ module.exports = function(app) {
 
   // Add commands
   app.cmd('help', '', helpdesk.config.channels, helpdesk.config.channelsToExclude, helpdesk.help);
+  app.cmd('list', '', helpdesk.config.channels, helpdesk.config.channelsToExclude, helpdesk.list);
 
   // PM commands
   app.msg('email', '', helpdesk.email);
   app.msg('help', '', helpdesk.help);
+  app.msg('list', '', helpdesk.list);
 };
